d730f3610ab6bdbacae9c0a2133b4c76949f48b2
HOC HUYNH <hthaihoc2029@gmail.com>
2024-03-31 20:06:45 +0700

    #20 setState

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -11,12 +11,44 @@ class MyComponent extends React.Component {
     age: 26,
   };
 
+  handleClick(event) {
+    console.log(">> clicked");
+    console.log(event);
+
+    //merge State => react Class
+    // unuse var in state unchange
+    this.setState({
+      name: "Huynh Thai Hoc",
+      age: Math.floor(Math.random() * (100 - 1 + 1)) + 1,
+    });
+  }
+  // Using arrow function when call normal function
+  handleOnMouseOver(event) {
+    // console.log(this.state.name);
+  }
+
   //JSX - viet code js trong html
   render() {
     return (
       <div>
         My name is {this.state.name} and I come from {this.state.address},{" "}
         {this.state.age} years old.
+        <div>
+          <button
+            onClick={(event) => {
+              this.handleClick(event);
+            }}
+          >
+            Click me!
+          </button>
+          <button
+            onMouseOver={(event) => {
+              this.handleOnMouseOver(event);
+            }}
+          >
+            Hover me!
+          </button>
+        </div>
       </div>
     );
   }
