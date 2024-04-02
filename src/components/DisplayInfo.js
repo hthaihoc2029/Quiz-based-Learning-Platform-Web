39c70e18dc342c4ec17fa532c5f2ecd0175ae52d
HOC HUYNH <hthaihoc2029@gmail.com>
2024-04-03 01:56:42 +0700

    #34 stateful stateless function vs class

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,80 +1,68 @@
 import React from "react";
 import "./DisplayInfo.scss";
-import logo from "../logo.svg";
-class DisplayInfo extends React.Component {
-  constructor(props) {
-    console.log(">> call constructor");
-    super(props);
-    //babel compiler
-    this.state = {
-      isShowListUser: true,
-    };
-  }
+// import logo from "../logo.svg";
 
-  componentDidMount() {
-    console.log(">> call componentDidMount");
-  }
+//stateless vs stateful
+// class DisplayInfo extends React.Component {
+//   render() {
+//     console.log("call me Render");
+//     // destructuring array/obj
+//     const { listUsers } = this.props;
 
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    console.log(">> call componentDidUpdate", this.props, prevProps);
+//     return (
+//       //props => properties
+//       <div className="display-info-container">
+//         {true && (
+//           <>
+//             {listUsers.map((user, index) => {
+//               return (
+//                 <div className={+user.age > 18 ? "red" : "green"} key={user.id}>
+//                   <div>My name is {user.name}</div>
+//                   <div>My age is {user.age}</div>
+//                   <div>
+//                     <button
+//                       onClick={() => this.props.handleDeleteUser(user.id)}
+//                     >
+//                       Delete
+//                     </button>
+//                   </div>
+//                   <hr />
+//                 </div>
+//               );
+//             })}
+//           </>
+//         )}
+//       </div>
+//     );
+//   }
+// }
 
-    if (this.props.listUsers !== prevProps.listUsers) {
-      if (this.props.listUsers.length === 5) {
-        alert("You have 5 user!");
-      }
-    }
-  }
+const DisplayInfo = (props) => {
+  const { listUsers } = props;
 
-  handleShowHide = (event) => {
-    this.setState({
-      isShowListUser: !this.state.isShowListUser,
-    });
-  };
+  console.log("call me Render");
 
-  render() {
-    console.log("call me Render");
-    // destructuring array/obj
-    const { listUsers } = this.props;
-
-    return (
-      //props => properties
-      <div className="display-info-container">
-        <img src={logo} alt="" />
-        <div>
-          <span
-            onClick={(event) => {
-              this.handleShowHide(event);
-            }}
-          >
-            {this.state.isShowListUser === true
-              ? "Hide list users:"
-              : "Show list users:"}
-          </span>
-        </div>
-
-        {this.state.isShowListUser && (
-          <>
-            {listUsers.map((user, index) => {
-              return (
-                <div className={+user.age > 18 ? "red" : "green"} key={user.id}>
-                  <div>My name is {user.name}</div>
-                  <div>My age is {user.age}</div>
-                  <div>
-                    <button
-                      onClick={() => this.props.handleDeleteUser(user.id)}
-                    >
-                      Delete
-                    </button>
-                  </div>
-                  <hr />
+  return (
+    <div className="display-info-container">
+      {true && (
+        <>
+          {listUsers.map((user, index) => {
+            return (
+              <div className={+user.age > 18 ? "red" : "green"} key={user.id}>
+                <div>My name is {user.name}</div>
+                <div>My age is {user.age}</div>
+                <div>
+                  <button onClick={() => props.handleDeleteUser(user.id)}>
+                    Delete
+                  </button>
                 </div>
-              );
-            })}
-          </>
-        )}
-      </div>
-    );
-  }
-}
-
+                <hr />
+              </div>
+            );
+          })}
+        </>
+      )}
+    </div>
+  );
+};
 export default DisplayInfo;
