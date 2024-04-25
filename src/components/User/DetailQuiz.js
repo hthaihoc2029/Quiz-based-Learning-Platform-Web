3b451eaeb3fbcff9d868b0613ad69ad6e8c0108c
HOC HUYNH <hthaihoc2029@gmail.com>
2024-04-26 05:35:00 +0700

    fetch question + lodash

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getDataQuiz } from "../../services/apiService";
+import _ from "lodash";
 
 const DetailQuiz = (props) => {
   const params = useParams();
@@ -12,7 +13,21 @@ const DetailQuiz = (props) => {
 
   const fetchQuestions = async () => {
     const res = await getDataQuiz(quizId);
-    console.log("check res:", res);
+    if (res && res.EC === 0) {
+      let raw = res.DT;
+      let data = _.chain(raw)
+        .groupBy("id")
+        .map((value, key) => {
+          let questionDesc = value[0].description;
+          let questionImg = value[0].image;
+          let answers = [];
+          value.forEach((item, index) => {
+            answers.push(item.answers);
+          });
+          return { questionId: key, answers, questionDesc, questionImg };
+        })
+        .value();
+    }
   };
 
   return <div className="detail-quiz-container">DetailQuiz</div>;
